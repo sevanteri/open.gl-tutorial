e95490c75cce4e8fdd6d0f3426e6bacc33aa290a
Pete Sevander <pete.sevander@liilak.com>
2015-04-13 15:56:24 +0300

    Wobbly kitty

--- fragment.glsl
+++ fragment.glsl
@@ -13,6 +13,18 @@ uniform float time;
 void main()
 {
     vec4 colKitty = texture(texKitty, Texcoord);
-    vec4 colPuppy = texture(texPuppy, Texcoord);
-    outColor = mix(colKitty, colPuppy, ((sin(time / 2) + 1.0) / 2.0));
+    //vec4 colPuppy = texture(texPuppy, Texcoord);
+    float a = 0.4;
+    if (Texcoord.y <= a) {
+        outColor = colKitty;
+    } else {
+        outColor = texture(texKitty,
+            vec2(
+                //sin(time) * Texcoord.x,
+                Texcoord.x + (sin(Texcoord.y * 60.0 + time/2) / 30.0) * (a - Texcoord.y),
+                a*2.0 - Texcoord.y
+            )
+        );
+    }
+    //outColor = mix(colKitty, colPuppy, ((sin(time / 2) + 1.0) / 2.0));
 }
